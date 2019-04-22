c23c5baffc33c4555e84de5e65e3bd87087709b0
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-22 10:40:00 -0500

    added input to binary, and now we print the version with the usage message.

--- cppBinary/main.cpp
+++ cppBinary/main.cpp
@@ -1,7 +1,20 @@
 
-#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "main.h"
 
-int main(int argc, char *argv[]){
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+int main (int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stdout,"%s Version %d.%d\n",
+                argv[0],
+                MainBinary_VERSION_MAJOR,
+                MainBinary_VERSION_MINOR);
+        fprintf(stdout,"Usage: %s name.\n",argv[0]);
+        return 1;
+    }
+
+    std::string name = argv[1];
+    fprintf(stdout, "Hello %s\n", name.c_str() );
+    return 0;
 }
